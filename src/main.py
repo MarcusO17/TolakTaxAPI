06c60266c2fa23c4aa9457141dd6610b08170217
MarcusO17 <marcusonggeye@gmail.com>
2025-05-19 15:08:24 +0800

    added upload-image post

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from groq import Groq
+from dotenv import load_dotenv
+import uuid
 import os
+import uvicorn 
+
+
+load_dotenv()
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
 
 app = FastAPI()
 
@@ -11,6 +18,16 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/image/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+
+    with open(file.filename, "wb") as f:
+        f.write(contents)
+
+    return {"filename": file.filename}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
